41f1a2e9510cf54de671f2d97cbae71869a82d08
Peter Nichols <peternichols2002@gmail.com>
2023-11-21 21:56:38 -0700

    Added Singleton Pattern
    We created diferrent type of syngleton. The safe singleton is the best to use in a multi-threading environment

---
+++ SingletonPattern/test/Singleton_test.cpp
@@ -0,0 +1,12 @@
+/*
+ * Singleton_test.cpp
+ *
+ *  Created on: Nov 7, 2023
+ *      Author: pierrebeldor
+ */
+
+#include "Singleton.h"
+
+namespace patterns {
+
+} /* namespace patterns */
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Singleton.h"
+#include "TestSingletonPattern.h"
+
+
+using namespace patterns;
+
+auto main(int argc, char* argv[]) ->int
+{
+	TestSingletonPattern::testSingletonSafe();
+
+
+	return 0;
+}
