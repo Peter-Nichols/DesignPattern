0be6626624e89d1fbada23a519582260fec9af2e
Peter Nichols <peternichols2002@gmail.com>
2025-01-10 21:23:09 -0700

    Added Unit tests using gtest for Factory Patterns

--- FactoryPattern/test/ServerComputer_test.cpp
+++ FactoryPattern/test/ServerComputer_test.cpp
@@ -7,6 +7,88 @@
 
 #include "ServerComputer.h"
 
-namespace patterns {
+#include <gtest/gtest.h>
 
-} /* namespace patterns */
+#include <string>
+class ServerComputerFixture : public ::testing::Test
+{
+protected:
+    patterns::ServerComputer* server;
+
+
+    void SetUp() override
+    {
+        server = new patterns::ServerComputer();
+        //server = new patterns::ServerComputer("10 GB", "500 GB", "2.5 GHz");
+    }
+
+    void TearDown() override
+    {
+        delete(server);
+        server = nullptr;
+    }
+};
+
+TEST_F(ServerComputerFixture, ServerComputerTests_Constructor)
+{
+    //patterns::ServerComputer* server = new patterns::ServerComputer(patterns::ComputerType::SERVER, "10 GB", "1 TB", "2.0 GHz");
+
+    EXPECT_TRUE(server != nullptr);
+
+    EXPECT_EQ(server->getCPU().compare(""), 0);
+    EXPECT_EQ(server->getHDD().compare(""), 0);
+    EXPECT_EQ(server->getRAM().compare(""), 0);
+
+    delete server;
+    server = nullptr;
+}
+
+TEST_F(ServerComputerFixture, ServerComputerTests_Constructor2)
+{
+    std::string ram = "10 GB";
+    std::string hdd = "1 TB";
+    std::string cpu = "2.0 GHz";
+    patterns::ServerComputer* serverComp = new patterns::ServerComputer(ram, hdd, cpu);
+
+    EXPECT_TRUE(serverComp != nullptr);
+
+    EXPECT_EQ(serverComp->getCPU().compare("2.0 GHz"), 0);
+    EXPECT_EQ(serverComp->getHDD().compare("1 TB"), 0);
+    EXPECT_EQ(serverComp->getRAM().compare("10 GB"), 0);
+
+    delete serverComp;
+    server = nullptr;
+}
+
+TEST_F(ServerComputerFixture, ServerComputerTests_SetRAM)
+{
+    std::string ram = "10 GB";
+
+    EXPECT_EQ(server->getRAM().compare(""), 0);
+
+    server->setRAM(ram);
+
+    EXPECT_EQ(server->getRAM().compare("10 GB"), 0);
+}
+
+TEST_F(ServerComputerFixture, ServerComputerTests_SetHDD)
+{
+    std::string hdd = "1 TB";
+
+    EXPECT_EQ(server->getHDD().compare(""), 0);
+
+    server->setHDD(hdd);
+
+    EXPECT_EQ(server->getHDD().compare("1 TB"), 0);
+}
+
+TEST_F(ServerComputerFixture, ServerComputerTests_SetCPU)
+{
+    std::string cpu = "2.0 GHz";
+
+    EXPECT_EQ(server->getCPU().compare(""), 0);
+
+    server->setCPU(cpu);
+
+    EXPECT_EQ(server->getCPU().compare("2.0 GHz"), 0);
+}
